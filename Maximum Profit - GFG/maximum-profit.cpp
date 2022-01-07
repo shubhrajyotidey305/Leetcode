ed635d250549c49a50ae6319ffbb9fa1202ef1a5
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-01-07 13:19:44 +0530

    Added solution - LeetHub

--- Maximum Profit - GFG/maximum-profit.cpp
+++ Maximum Profit - GFG/maximum-profit.cpp
@@ -14,21 +14,15 @@ class Solution {
         
         vector<vector<int>> dp(k+1, vector<int>(n, 0));
         
+        
         for(int t=1; t<=k; t++){
+            int maxi = INT_MIN;
             for(int d=1; d<n; d++){
-                int maxi = dp[t][d-1];
                 
+                maxi = max(maxi, dp[t-1][d-1] - a[d-1]);
                 
-                for(int pd = 0; pd<d; pd++){
-                    int pf = dp[t-1][pd];
-                    int pth = a[d] - a[pd];
-                    
-                    if(pf + pth > maxi){
-                        maxi = pf + pth;
-                    }
-                }
+                dp[t][d] = max(dp[t][d-1], maxi+a[d]);
                 
-                dp[t][d] = maxi;
             }
         }
         
