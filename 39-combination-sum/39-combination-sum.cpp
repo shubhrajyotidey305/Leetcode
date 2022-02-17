8f2e19360eeb9fb5c3a1781b16ca991e08b36dbf
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-02-17 13:19:22 +0530

    Time: 126 ms (19.71%), Space: 17.1 MB (33.18%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:     
+    vector<vector<int>> combinationSum(vector<int>& c, int t) {    
+        vector<int> res;
+        vector<vector<int>> ans;        
+        dfs(0, c, t, res, ans);   
+        
+        for(auto &i:ans){
+            sort(i.begin(), i.end());
+        }
+        
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        
+        return ans;
+    }
+    
+    void dfs(int sum, vector<int>& c, int &t, vector<int> &res, vector<vector<int>> &ans){
+        if(sum == t){
+            ans.push_back(res);
+            return;
+        }
+        
+        if(sum > t){
+            return;
+        }
+        
+        for(auto it:c){
+            res.push_back(it);
+            dfs(sum+it, c, t, res, ans);
+            res.pop_back();
+        }
+    }
+};
