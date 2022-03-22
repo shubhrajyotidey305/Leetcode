e02fde0e18672a37051f3861eca34ee1f5642d38
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-03-22 11:45:52 +0530

    Time: 101 ms (20.79%), Space: 21.5 MB (63.46%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        map<int, char> mp;
+        unordered_map<int, char> mp;
         for(int i=1; i<=26; i++){
             mp[i] = (char) 'a' + i - 1;
         }
