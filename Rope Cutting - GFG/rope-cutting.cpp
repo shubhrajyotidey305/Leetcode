c3dc09a17069879c26565ee75b47ea7829c1c9da
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-05-16 17:06:19 +0530

    Added solution - LeetHub

---
+++ Rope Cutting - GFG/rope-cutting.cpp
@@ -0,0 +1,50 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution{
+    public:
+        vector<int> RopeCutting(int arr[], int n)
+        {
+            // Complete the function
+            sort(arr, arr+n);
+            vector<int> v;
+            int i = 0;
+            while(i < n){
+                int minimum = arr[i];
+                while(arr[i] == minimum) i++;
+                int r = n-i;
+                if(r != 0) v.push_back(r);
+            }
+            return v;
+        }
+};
+
+// { Driver Code Starts.
+int main()
+{   
+    int T;
+    cin>> T;
+    while(T--)
+    {
+       	int n;
+       	cin>>n;
+       	int arr[n+1];
+       	for(int i=0;i<n;i++)
+       	   cin>>arr[i];
+        
+        vector<int> v;
+        Solution ob;
+        v = ob.RopeCutting(arr, n);
+        for(int i : v){
+            cout << i << " ";
+        }
+        cout << endl;
+        
+        
+    }
+  return 0;
+}  // } Driver Code Ends
