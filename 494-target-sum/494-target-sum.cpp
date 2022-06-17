908e9636d0093d94169245ee56a1c2601d5f85e5
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-06-17 17:04:29 +0530

    Time: 14 ms (77.72%), Space: 9.3 MB (55.96%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,8 +1,6 @@
 class Solution {
-public:
-    int cnt = 0;
-    int dp[22][4002]; 
-    // n => 22, target + 1000 => 2002
+public:    
+    int dp[22][4002];     
     int solve(vector<int>& nums, int target, int n) {
         if(n == 0){
             if(target == 0) return 1;
@@ -11,8 +9,7 @@ public:
         
         if(dp[n][target + 2000]!=-1){            
             return dp[n][target + 2000];
-        }
-        
+        }       
         
         return dp[n][target + 2000] = solve(nums, target-nums[n-1], n-1) + solve(nums, target+nums[n-1], n-1);                     
         
