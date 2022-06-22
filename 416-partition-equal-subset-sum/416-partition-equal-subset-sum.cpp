aa5b1d5aba5e4230954061a01e87da03b3d945c1
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-06-22 21:08:48 +0530

    Time: 95 ms (87.97%), Space: 199.7 MB (5.12%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -6,19 +6,20 @@ public:
         int total = 0;
         for(int i=0; i<n; i++) total += nums[i];
         dp = vector<vector<int>> (n+1, vector<int>(total+1, -1));
-        return solve(n, 0, total, nums);
+        
+        if(total & 1) return false;        
+        total /= 2;
+        return solve(n, total, nums);       
+        
     }
     
-    bool solve(int n, int sum, int total, vector<int> &nums){
-        if(n == 0){
-            return sum == total - sum;
-        }   
-        
-        if(sum == total - sum){
-            return true;
+    bool solve(int n, int sum, vector<int> &nums){
+        if(n == 0 or sum == 0){
+            return sum == 0 ? true : false;
         }
         
-        if(dp[n][sum]!=-1) return dp[n][sum];    
-        return dp[n][sum] = (solve(n-1, sum+nums[n-1], total, nums) || solve(n-1, sum, total, nums));
+        if(dp[n][sum]!=-1) return dp[n][sum];
+        if(nums[n-1]<=sum) return dp[n][sum] = solve(n-1, sum-nums[n-1], nums) or solve(n-1, sum, nums);
+        else return dp[n][sum] = solve(n-1, sum, nums);
     }
 };
