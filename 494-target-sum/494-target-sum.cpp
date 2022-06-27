0d04dfe1d8e1b1db16c79e482cfb659066699dbc
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-06-27 21:00:59 +0530

    Time: 40 ms (46.02%), Space: 12.3 MB (39.41%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,24 +1,35 @@
 class Solution {
-public:    
-    int dp[22][4002];     
-    int solve(vector<int>& nums, int target, int n) {        
-        if(n == 0){
-            if(target == 0) return 1;
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int total = 0;
+        for(auto &it:nums) total += it;
+        target = abs(target);
+        if (target > total || ((target + total)&1))
             return 0;
-        }       
+        int sum1 = (total + target) / 2;       
         
-        if(dp[n][target + 2000]!=-1){            
-            return dp[n][target + 2000];
-        }       
-        
-        return dp[n][target + 2000] = solve(nums, target-nums[n-1], n-1) + solve(nums, target+nums[n-1], n-1);                     
-        
+        return countSubset(nums, sum1);
     }
     
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        int x = solve(nums, target, n);       
-        return x;
+    int countSubset(vector<int> &arr, int sum) {
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
+        dp[0][0] = 1;
+        for(int i=1; i<=n; i++){
+            if(arr[i-1] == 0) dp[i][0] = 2*dp[i-1][0];
+            else dp[i][0] = dp[i-1][0];
+        }
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=sum; j++){
+                if(arr[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-arr[i-1]] + dp[i-1][j];
+                }
+                else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum];
     }
 };
