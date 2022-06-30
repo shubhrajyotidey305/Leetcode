37816a4140679def343d263120cd84282b660f4f
Shubhrajyoti Dey <56960378+shubhrajyotidey305@users.noreply.github.com>
2022-07-01 00:47:30 +0530

    Time: 51 ms (22.03%), Space: 10.9 MB (58.38%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -9,16 +9,10 @@ public:
     int solve(string &s1, string &s2, int n, int m){
         if(n==0 or m==0) return 0;
         
-        if(s1[n-1] == s2[m-1]) {
-            if(dp[n][m] != -1) return dp[n][m];
-            else return dp[n][m] = 1 + solve(s1, s2, n-1, m-1);
-        }
+        if(dp[n][m] != -1) return dp[n][m];
         
-        else {
-            if(dp[n][m] != -1) return dp[n][m];
-            else return dp[n][m] =  max(solve(s1, s2, n-1, m),solve(s1, s2, n, m-1));
-        }           
-            
+        if (s1[n-1] == s2[m-1]) return dp[n][m] = 1 + solve(s1, s2, n-1, m-1); 
+        else return dp[n][m] =  max(solve(s1, s2, n-1, m),solve(s1, s2, n, m-1));       
         
     }
 };
