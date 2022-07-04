2fc2762a1a81bf6be918e4e1aea1098b6cd3d93c
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-07-04 23:18:04 +0530

    Added solution - LeetHub

---
+++ Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
@@ -0,0 +1,61 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minVal(int a, int b) {
+        // code here
+        int n = __builtin_popcount(b);
+        bitset<32> set(a);
+        bitset<32> num;
+        for(int i=31; i>=0; i--) {
+            if(set[i] == 1){
+                if(n > 0) {
+                    num[i] = 1;
+                    n--;
+                }
+                
+            }
+            
+            if(n == 0) break;
+        }
+        
+        int i = 0;
+        while(n) {
+            if(num[i] == 0){
+                num[i] = 1;
+                n--;
+            }
+            i++;
+        }
+        
+        // for(int i=31; i>=0; i--) cout << num[i] << " ";
+        // cout << endl;
+        
+        return (int) num.to_ulong();
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        Solution ob;
+        cout << ob.minVal(a, b);
+
+        cout << "\n";
+    }
+
+    return 0;
+}  // } Driver Code Ends
