510f6072f19dc20cb4617949e81cdb26f64418d1
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-08-25 20:16:03 +0530

    Time: 3 ms (84.81%), Space: 7.8 MB (44.50%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = a.size();
         int m = a[0].size();     
         
-        if(a[n-1][m-1] == 1) return 0;
+        if(a[0][0] == 1 or a[n-1][m-1] == 1) return 0;
         
         vector<int> dp (m);
         dp[0] = 1;
@@ -17,8 +17,8 @@ public:
             for(int j=0; j<m; j++) {
                 if(i == 0 and j == 0) continue;            
                 int op1 = 0,  op2 = 0;
-                if(i > 0 and a[i-1][j] != 1) op1 = dp[j];
-                if(j > 0 and a[i][j-1] != 1) op2 = t[j-1];
+                if(i > 0 and a[i][j] != 1) op1 = dp[j];
+                if(j > 0 and a[i][j] != 1) op2 = t[j-1];
                 t[j] = (op1 + op2);            
             }
             dp = t;
