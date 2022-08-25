b1bb47ba150c32caaf4eee995c139b7b03ad1d34
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-08-25 20:00:32 +0530

    Time: 7 ms (39.85%), Space: 7.7 MB (66.01%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,35 +1,28 @@
 class Solution {
-public:
+public:    
     int uniquePathsWithObstacles(vector<vector<int>>& a) {
-        int m = a.size();
-        int n = a[0].size();
+        int n = a.size();
+        int m = a[0].size();     
         
-        long long dp[m][n];
-        memset(dp, 0, sizeof(dp));
+        if(a[n-1][m-1] == 1) return 0;
         
-        if(a[m-1][n-1] == 0)
-            dp[m-1][n-1] = 1;
-        
-        for(int i = n-2; i>=0; i--){
-            if(a[m-1][i] == 0){
-                dp[m-1][i] += dp[m-1][i+1];    
+        vector<int> dp (m);
+        dp[0] = 1;
+
+        for(int i=0; i<n; i++) {
+            vector<int> t(m);
+            if(i == 0) {
+                t[0] = 1;
+            }        
+            for(int j=0; j<m; j++) {
+                if(i == 0 and j == 0) continue;            
+                int op1 = 0,  op2 = 0;
+                if(i > 0 and a[i-1][j] != 1) op1 = dp[j];
+                if(j > 0 and a[i][j-1] != 1) op2 = t[j-1];
+                t[j] = (op1 + op2);            
             }
-            
-        }
-        
-        for(int i = m-2; i>=0; i--){
-            if(a[i][n-1] == 0){
-                dp[i][n-1] += dp[i+1][n-1];    
-            }            
-        }
-        
-        for(int i=m-2; i>=0; i--){
-            for(int j=n-2; j>=0; j--){
-                if(a[i][j] == 0){
-                    dp[i][j] += dp[i+1][j] + dp[i][j+1];
-                }
-            }
-        }
-        return dp[0][0];
+            dp = t;
+        }    
+        return dp[m-1];
     }
 };
