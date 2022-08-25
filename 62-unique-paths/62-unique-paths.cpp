26d1eeab7873479b4a14383f32c24c6f4268e311
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-08-25 14:32:13 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (54.88%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,35 +1,27 @@
 class Solution {
 public:
-    int dp[102][102];
-    int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(0, 0, m, n);
-    }
-    
-    int solve(int i, int j, int m, int n) {
-        if(i == m-1 and j == n-1) {
-            return 1;
+    int uniquePaths(int a, int b) {
+        vector<int> dp(b,0);
+        dp[b-1] = 1;
+
+        for(int i=a-1; i>=0; i--) {
+            vector<int> temp(b,0);
+            for(int j=b-1; j>=0; j--) {
+                if(i == a-1 and j == b-1) {
+                    temp[j] = 1;
+                    continue;
+                }
+                int op1 = 0, op2 = 0;
+                if(i+1 < a) {
+                    op1 = dp[j];
+                }
+                if(j+1 < b) {
+                    op2 = temp[j+1];
+                }
+                temp[j] = op1 + op2;
+            }
+            dp = temp;
         }
-        
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int l = -1, r = -1;
-        if(isSafe(i+1, j, m, n)) {
-            l = solve(i+1, j, m, n);
-        }
-        
-        if(isSafe(i, j+1, m, n)) {
-            r = solve(i, j+1, m, n);
-        }
-        
-        if(l == -1 and r != -1) return dp[i][j] = r;
-        if(l != -1 and r == -1) return dp[i][j] = l;
-        if(l != -1 and r!= -1) return dp[i][j] = l+r;
-        return dp[i][j] = 0;     
-        
-    }
-    
-    bool isSafe(int i, int j, int m, int n) {
-        return (i >= 0 and i < m and j >=0 and j < n);
+        return dp[0];
     }
 };
