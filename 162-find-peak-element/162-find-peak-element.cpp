ee0d4b5be107c1c72705198bb81e1cbb6b99d808
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-09-02 16:30:33 +0530

    Time: 4 ms (77.84%), Space: 8.9 MB (51.34%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,55 +1,14 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n = nums.size();
-        int low = 0 ;
-        int high = n - 1 ;
-        
-        if(n == 1) return 0;
-        
-        while(low<=high){
-            
-            long long mid = low + (high-low)/2 ;
-            
-            //if peak is in starting point
-            if(mid == 0){
-                
-                if(nums[0] > nums[1]){
-                    return 0;
-                }else{
-                    return 1 ;
-                }
-                
-            }
-            
-            //if peak is in between array
-            else if(mid > 0 && mid < n - 1){
-                
-                //if mid is greater than both side than mid is peak
-                
-                if(nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]){
-                    return mid ;
-                }
-                
-                else if(nums[mid-1] > nums[mid]){
-                    high = mid - 1 ;
-                }
-                
-                else{
-                    low = mid + 1 ;
-                }
-            }
-            
-              // if peak is in end point
-            
-            else if(mid == n-1){
-                if(nums[n - 1] > nums[n - 2]){
-                    return n - 1 ;
-                }else{
-                    return n - 2 ;
-                }
-            }
-        }
-        return 0;
+        return solve(nums, 0, nums.size()-1, nums.size());
+    }
+    
+    int solve(vector<int> &nums, int low, int high, int n) {
+        int mid = low + (high - low) / 2;
+        if((mid == 0 || nums[mid - 1] <= nums[mid]) && (mid == n - 1 || nums[mid + 1] <= nums[mid]))
+            return mid;
+        else if(mid > 0 && nums[mid - 1] > nums[mid]) return solve(nums, low, mid-1, n);
+        else return solve(nums, mid+1, high, n);
     }
 };
