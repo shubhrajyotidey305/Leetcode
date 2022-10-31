8aaf145f1b82e30d81c664b05e68ada43be75858
Shubhrajyoti Dey <shubhrajyotidey305@gmail.com>
2022-10-31 11:01:07 +0530

    Time: 34 ms (41.03%), Space: 17.3 MB (85.40%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& m) {
+        for (int i = 1; i < m.size(); i++)
+            for (int j = 0; j < m[0].size()-1; j++)
+                if (m[i-1][j] != m[i][j+1]) return false;
+        return true;
+    }
+};
